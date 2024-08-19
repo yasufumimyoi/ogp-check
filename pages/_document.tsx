d753368e3f7a52e3622e227ac29ba96a0919ebfd
yasufumimyoi <yasufumi.myoi@gmail.com>
2024-08-19 23:09:05 +0900

    feat:first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,37 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { DefaultSeo, NextSeo } from "next-seo";
 
 export default function Document() {
   return (
     <Html lang="en">
+      {/* <DefaultSeo
+        defaultTitle={"TEST"}
+        description={"DESCRIPTION"}
+        openGraph={{
+          type: "article",
+          title: "OG TITLE",
+          description: "OG DESC",
+          site_name: "SITE NAME",
+          url: "https://www.schole-corp.com",
+          images: [
+            {
+              url: "https://www.schole-corp.com/images/common/meta/ogp.jpg",
+              width: 1200,
+              height: 630,
+              alt: "FDKSJFSLD",
+              type: "image/jpeg",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+        facebook={{
+          appId: "517270843849718",
+        }}
+      /> */}
       <Head />
       <body>
         <Main />
